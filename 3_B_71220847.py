b0a0d53197e96525f0e331eb667724fc446e522e
eric-vinn <eric.vincent@ti.ukdw.ac.id>
2022-11-17 14:39:17 +0700

    commitcommit

--- 3_B_71220847.py
+++ 3_B_71220847.py
@@ -1,9 +1,34 @@
 print ("========== CATATAN BELANJA ==========")
-print ("========== Daftar 1 ==========")
 
+print ("========== Daftar 1 ==========")
 daftarBelanja1 = ['Sikat Gigi', 'Odol', 'Shampoo', 'Sabun', 'Ciduk']
 print ("Daftar Belanja 1: ", daftarBelanja1)
+print ("")
 
+print ("========== Daftar 2 ==========")
 daftarBelanja2 = ['Teh', 'Gula', 'Garam', 'Micin', 'Kecap']
 print ("Daftar Belanja 2: ", daftarBelanja2)
+print ("")
 
+print ("Jawab dengan angka [1/2]")
+print ("1. Rubah Belanjaan")
+print ("2. Keluar")
+
+a = input("Masukan Pilihan ")
+if a == 2:
+    print
+else:
+    if a == 1:
+        b = input("Masukan nama item ke daftar 1 : ")
+        c = int(input("Masukan index yang di ingin di rubah : "))
+        daftarBelanja1[c]=b
+        d = input("Masukan nama item ke daftar 2 : ")
+        e = int(input("Masukan index yang ingin dirubah : "))
+        daftarBelanja2[e]=d
+        print("")
+        print("========== Daftar 1 ==========")
+        print("Daftar Belanja 1: ", daftarBelanja1)
+        print("")
+        print("Daftar Belanja 2: ", daftarBelanja2)
+    else:
+        print("Wrong Input")
--- 4_B_71220847.py
+++ 4_B_71220847.py
@@ -1,7 +1,73 @@
-namaPlayer = int(input("Nama: "))
-print ("==========================")
-print ("<<<<<<<<<<<<<<<<<POPOPOPO>>>>>>>>>>>>>>>>>>>>>")
-print ("Welcome banh")
+playerName = input("STATE YOUR NAME: ")
+print ("")
+print ("==============================================")
+print ("")
+print ("<<<<<<<<<<<<<<<<< Adventure of", playerName, ">>>>>>>>>>>>>>>>>>>>>")
+print ("")
+print ("Welcome to the Adventure of ", playerName, "!")
+print ("")
+print ("Are you a Male / Female? [M/F]")
+gender = input(">>")
 
-kelamin = int(input("Cowo/Cewe? {Cwo,Cwe]"))
+if gender == "M":
+    print
+else:
+    if gender == "F":
+        print
+    else:
+        print ("Please choose betweeen M or F")
+            
 
+print ("One day, a young adventurer named", playerName)
+print ("Spawned in a peacefull Village of Orion")
+print (playerName, "walked out of Village and see a slime jumping around.")
+print ("")
+print ("picked up a... (Sword/Bow)")
+
+weapon = input ("")
+if weapon == "Sword":
+    print ("and attack the slime.")
+    print ("the slime get sliced and died. adventurer gain a level")
+else:
+    if weapon == "Bow":
+        print ("and shot the slime.")
+        print ("the slime get shotted and died. adventurer gain a level")
+    else:
+        print ("Please choose between Sword or Bow")
+
+print ("")
+
+print (playerName, "The Adventurer, then run towards a chest that glows from the inside")
+print ("PICK ONE!") 
+print ("(Open/Walk)")
+chest = input ("")
+if chest == "Open":
+    print ("The Adventurer Finds a Gold and brings it Home to the Village.")
+else:
+    if chest == "Walk":
+        print ("The Adventurer walk pass the chest to the Village")
+    else:
+        print ("Please choose between Open or Walk")
+print("")
+print("as the advenure walks, he startd to feel more and more tired.")
+print("take a [Taxi] / Keep [Walk]ing")
+walkTaxi = input("")
+if walkTaxi == "Taxi":
+    print ("The Adventurer choose to walk.")
+else:
+    if walkTaxi == "Walk":
+        print ("The Adventurer choose to take a taxi")
+    else:
+        print ("Please choose between Taxi or Walk")
+
+print (playerName, "Arrivevd at the village and see RTX 3090 TI on sale")
+print ("With the price of 1 gold")
+print ("[Buy/Pass]")
+rtx = input("")
+if rtx == "Buy":
+    print ("Congratulation you got RTX 3090 TI!")
+else:
+    if rtx == "Pass":
+        print ("Sad, you dont get that RTX 3090 TI")
+    else:
+        print ("Choose between Buy/Pass")
